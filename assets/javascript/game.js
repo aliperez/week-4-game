c7e23837daea1599b949c912b49d77c0e07bb1c9
aliperez <alicia.perez@utexas.edu>
2018-03-17 13:13:27 -0700

    functional js and jq, ugly css

---
+++ assets/javascript/game.js
@@ -0,0 +1,94 @@
+var targetNum;
+var playerScore;
+var gem1;
+var gem2;
+var gem3;
+var gem4;
+var wins = 0;
+var losses = 0;
+
+var init = function(){
+    playerScore = 0;
+    $("#yourScore").text(playerScore);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+
+    gem1 = Math.floor(Math.random()*12) + 1;
+    gem2 = Math.floor(Math.random()*12) + 1;
+    gem3 = Math.floor(Math.random()*12) + 1;
+    gem4 = Math.floor(Math.random()*12) + 1;
+
+    targetNum = Math.floor(Math.random()*102) + 19;
+    $("#target").text(targetNum);
+};
+
+//Testing purposes
+init();
+console.log(gem1);
+console.log(gem2);
+console.log(gem3);
+console.log(gem4);
+console.log(targetNum);
+
+
+$("#first-gem").on("click", function(){
+    playerScore = playerScore + gem1;
+    $("#yourScore").text(playerScore);
+    if (playerScore === targetNum){
+        alert("You win!");
+        wins ++;
+        init();
+    };
+    if (playerScore > targetNum){
+        alert("You lose!");
+        losses ++;
+        init();
+    };
+});
+
+$("#second-gem").on("click", function(){
+    playerScore = playerScore + gem2;
+    $("#yourScore").text(playerScore);
+    if (playerScore === targetNum){
+        alert("You win!");
+        wins ++;
+        init();
+    };
+    if (playerScore > targetNum){
+        alert("You lose!");
+        losses ++;
+        init();
+    };
+});
+
+$("#third-gem").on("click", function(){
+    playerScore = playerScore + gem3;
+    $("#yourScore").text(playerScore);
+    if (playerScore === targetNum){
+        alert("You win!");
+        wins ++;
+        init();
+    };
+    if (playerScore > targetNum){
+        alert("You lose!");
+        losses ++;
+        init();
+    };
+});
+
+$("#fourth-gem").on("click", function(){
+    playerScore = playerScore + gem4;
+    $("#yourScore").text(playerScore);
+    if (playerScore === targetNum){
+        alert("You win!");
+        wins ++;
+        init();
+    };
+    if (playerScore > targetNum){
+        alert("You lose!");
+        losses ++;
+        init();
+    };
+});
+
+
